151a9c5497c52fd99dcf9139901fb070e0777c25
David <112674521+DavidxDl@users.noreply.github.com>
2023-11-14 19:03:17 +0000

    YEP i had to delete everything again YAY

--- src/tests/Gameboard_placeShip.test.ts
+++
@@ -1,9 +0,0 @@
-import { Gameboard } from "../Gameboard";
-import { Ship } from "../Ship";
-
-test('Is placing ships correctly', () => {
-    const gameboard: Gameboard = new Gameboard(5);
-    gameboard.placeShip([3,3]);
-    console.log(gameboard)
-    expect(gameboard.boardGraph.get([3,3])).toBe(Ship)
-})
