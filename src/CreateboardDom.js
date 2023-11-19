a88c45cbf421848f1e3e131cf512a5181761b6d7
DavidxDl <Davidbekamlopez@gmail.com>
2023-11-19 20:31:02 +0000

    refactoring some of the index.js

--- src/CreateboardDom.js
+++ src/CreateboardDom.js
@@ -1,21 +1,23 @@
 import { Gameboard } from "./Gameboard.js";
 
-export function createGameboardDom(boardSize) {
-    const gameboard = new Gameboard(boardSize);
-    const domBoard = document.getElementById('board');
-    for (let x = 0; x < gameboard.boardSize; x++) {
-        for (let y = 0; y < gameboard.boardSize; y++) {
-            domBoard.append(createBoardSquare(`x${x}y${y}`));
-        }
+export function createGameboardDom(boardSize, id) {
+  const gameboard = new Gameboard(boardSize);
+  const wrapper = document.createElement("div");
+  wrapper.id = id;
+  const domBoard = document.getElementById("board");
+  for (let x = 0; x < gameboard.boardSize; x++) {
+    for (let y = 0; y < gameboard.boardSize; y++) {
+      wrapper.append(createBoardSquare(`x${x}y${y}`));
     }
-    return gameboard
-
+  }
+  document.getElementById("boards").append(wrapper);
+  return gameboard;
 }
 
 export function createBoardSquare(id) {
-    const square = document.createElement('div');
-    square.classList.add('boardSquare');
-    square.id = id;
+  const square = document.createElement("div");
+  square.classList.add("boardSquare");
+  square.id = id;
 
-    return square;
+  return square;
 }
--- src/index.js
+++ src/index.js
@@ -1,64 +1,66 @@
-import style from './style/style.css'
+import style from "./style/style.css";
 import { Gameboard } from "./Gameboard.js";
 import { Ship } from "./Ship.js";
 import { createGameboardDom, createBoardSquare } from "./CreateboardDom.js";
-import ComputerAi from './ComputerAi.js';
+import ComputerAi from "./ComputerAi.js";
 
-const body = document.querySelector('body');
-const domBoard = document.getElementById('board');
-const nodes = document.querySelectorAll('.boardSquare');
-const h1 = document.querySelector('h1');
-const h3 = document.querySelector('#shipsLeft');
+const body = document.querySelector("body");
+const domBoards = document.getElementById("boards");
+const nodes = document.querySelectorAll(".boardSquare");
+const h1 = document.querySelector("h1");
+const domPlayerShipsLeft = document.querySelector("#playerShipsLeft");
+const domCpuShipsLeft = document.querySelector("#cpuShipsLeft");
 
 const player = { isPlayersTurn: true };
-const playerBoard = createGameboardDom(7);
-playerBoard.placeShip([0,0], 3);
-playerBoard.placeShip([3,3], 5);
+const playerBoard = createGameboardDom(7, "playerBoard");
+playerBoard.placeShip([0, 0], 3);
+playerBoard.placeShip([3, 3], 5);
+const cpuBoard = createGameboardDom(playerBoard.boardSize, "cpuBoard");
+cpuBoard.placeShip([0, 0], 3);
+cpuBoard.placeShip([3, 3], 2);
+cpuBoard.placeShip([5, 2], 1);
 
 const computer = new ComputerAi(playerBoard.boardSize);
 
+domPlayerShipsLeft.innerText = "PLayer Ships left: " + playerBoard.shipsLeft;
 
+domBoards.addEventListener("click", (e) => {
+  if (player.isPlayersTurn) {
+    const cordX = e.target.id[1];
+    const cordY = e.target.id[3];
 
-h3.innerText = 'Ships left: ' + playerBoard.shipsLeft
-
-domBoard.addEventListener('click', (e) => {
-    if (player.isPlayersTurn) {
-        const cordX = e.target.id[1];
-        const cordY = e.target.id[3];
-
-        if(playerBoard.recieveAttack([cordX, cordY]) ===  true) {          // hit the shot
-            e.target.id = 'hit';
-            player.isPlayersTurn = false;
-            h3.innerText = 'Ships left: ' + playerBoard.shipsLeft;
-            if (playerBoard.shipsLeft === 0) {
-                h1.innerText = 'Game Over!';
-            }
-        } else if (playerBoard.recieveAttack([cordX, cordY]) ===  false) { // missed the shot
-            e.target.id = 'missed';
-            player.isPlayersTurn = false;
-        }
+    if (playerBoard.recieveAttack([cordX, cordY]) === true) {
+      // hit the shot
+      e.target.id = "hit";
+      player.isPlayersTurn = false;
+      domPlayerShipsLeft.innerText = "Cpu Ships left: " + playerBoard.shipsLeft;
+      if (playerBoard.shipsLeft === 0) {
+        h1.innerText = "Game Over!";
+      }
+    } else if (playerBoard.recieveAttack([cordX, cordY]) === false) {
+      // missed the shot
+      e.target.id = "missed";
+      player.isPlayersTurn = false;
     }
-})
-
+  }
+});
 
 setInterval(() => {
-    if(!player.isPlayersTurn) {
-        const randomCords = computer.getRandomCords();
-        if (playerBoard.recieveAttack(randomCords) === true) {
-            document.getElementById(`x${randomCords[0]}y${randomCords[1]}`).id = 'hit';
-            player.isPlayersTurn = true;
-            h3.innerText = 'Ships left: ' + playerBoard.shipsLeft;
-            if (playerBoard.shipsLeft === 0) {
-                h1.innerText = 'Game Over!';
-            } 
-        } else if (playerBoard.recieveAttack(randomCords) ===  false) { // missed the shot
-            document.getElementById(`x${randomCords[0]}y${randomCords[1]}`).id = 'missed';
-            player.isPlayersTurn = true;
-        }
+  if (!player.isPlayersTurn) {
+    const randomCords = computer.getRandomCords();
+    if (playerBoard.recieveAttack(randomCords) === true) {
+      document.getElementById(`x${randomCords[0]}y${randomCords[1]}`).id =
+        "hit";
+      player.isPlayersTurn = true;
+      domPlayerShipsLeft.innerText = "Ships left: " + playerBoard.shipsLeft;
+      if (playerBoard.shipsLeft === 0) {
+        h1.innerText = "Game Over!";
+      }
+    } else if (playerBoard.recieveAttack(randomCords) === false) {
+      // missed the shot
+      document.getElementById(`x${randomCords[0]}y${randomCords[1]}`).id =
+        "missed";
+      player.isPlayersTurn = true;
     }
+  }
 }, 1000);
-
-
-
-
-
